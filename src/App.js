f4cf6cdab0ad398eb424c54ff2d4b3a1568ee670
FlyaginAS <rus.reg.36@gmail.com>
2021-07-13 11:55:00 +0300

    3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit555555 <code>src/App.js</code> and save to reload.
         </p>
         <p>Hello from gh pages and react</p>
         <a
